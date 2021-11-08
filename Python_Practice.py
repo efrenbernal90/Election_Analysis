5299ef08ee1c2693b9eccd9e91838c431d9568f9
Efren Bernal <efrenbernal90@gmail.com>
2021-11-08 11:59:56 -0800

    update

--- Python_Practice.py
+++ Python_Practice.py
@@ -1,10 +1,5 @@
-counties_dict = {"Arapahoe": 422829, 
-                "Denver": 463353, 
-                "Jefferson": 432438}
-
-for keys in counties_dict:
-        print(keys)
-
-my_votes = int(input("How many votes did you get in the election? "))
-total_votes = int(input("what is the total votes in the election? "))
-print(f"I receieved {my_votes / total_votes *100}% of the total votes.")
+counties_dict= {"Arapahoe":422829,
+                "Denver":463353,
+                "Jefferson":432438} 
+for county in counties_dict.keys():
+        print(county)
