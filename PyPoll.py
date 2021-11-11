b3ae131a2e0a74e4d6eb07fd1a19181de146c748
Efren Bernal <efrenbernal90@gmail.com>
2021-11-10 18:28:42 -0800

    updated code to include winning  candidate names and voce coubt and percentage

--- PyPoll.py
+++ PyPoll.py
@@ -64,8 +64,9 @@ with open(file_to_load) as election_data:
         # Calculate the percentage of votes
         vote_percentage = float(votes) / float(total_votes) * 100
 
-        #Print the candidate name and percentage of votes.
-        print(f"{candidate_name} received {vote_percentage:.2f}% of the vote. ")
+        #  To do: print out the winning candidate, vote count and percentage to
+        #  terminal.
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
         # Determine winning vote count and candidate
         # Determine if the votes is greater than the winning count.
@@ -75,12 +76,12 @@ with open(file_to_load) as election_data:
             winning_count = votes
             winning_percentage = vote_percentage
             # And, set the winning_candidate equal to the candidate's name.
-            winning_candidate = candidate_name
+            winning_candidate = candidate_name 
 
-    #  To do: print out the winning candidate, vote count and percentage to
-    #  terminal.
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
-# Print total votes
-print(candidate_votes)
-
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
